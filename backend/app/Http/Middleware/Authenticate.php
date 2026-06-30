46dfc20e9fd5bf17caacc40a921c889d4bd63183
Willian Batista <willianmoreira.2000.19@gmail.com>
2026-06-30 15:17:36 -0300

    fix: dynamic cors allowed origins

--- backend/app/Http/Middleware/Authenticate.php
+++ backend/app/Http/Middleware/Authenticate.php
@@ -9,6 +9,6 @@ class Authenticate extends Middleware
 {
     protected function redirectTo(Request $request): ?string
     {
-        return null;
+        return $request->expectsJson() ? null : route('login');
     }
 }
